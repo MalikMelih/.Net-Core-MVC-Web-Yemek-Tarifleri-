2b95efbd6f2c816a1189c75d0b280e654f8fe256
melih <melih@DELL-WORK>
2022-12-09 05:59:14 +0300

    Add project files.

---
+++ OrnekUygulama/Controllers/YonetimController.cs
@@ -0,0 +1,242 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OrnekUygulama.Models;
+
+namespace OrnekUygulama.Controllers
+{
+    public class YonetimController : Controller
+    {
+        YemektarifleriDbContext db = new YemektarifleriDbContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Kullanicilar()
+        {
+            return View();
+        }
+        public IActionResult Bilgilerim()
+        {
+            return View();
+        }
+
+        //                                              <!-                            Sayfalar START                             -!>
+        //                                              <!-                            Sayfalar START                             -!>
+
+        public IActionResult Sayfalar()
+        {
+            var sayfalar = db.Sayfalars.Where(s => s.Silindi == false).OrderBy(s=>s.Baslik).ToList();
+
+            return View(sayfalar);
+        }
+        public IActionResult SayfaEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SayfaEkle(Sayfalar s)
+        {
+            s.Silindi = false;
+            db.Sayfalars.Add(s);
+            db.SaveChanges();
+            return RedirectToAction("Sayfalar");
+        }
+        public IActionResult SayfaGetir(int id)
+        {
+            var sayfa = db.Sayfalars.Where(s => s.Silindi == false && s.SayfaId == id).FirstOrDefault();
+
+            return View("SayfaGuncelle",sayfa);
+        }
+        public IActionResult SayfaGuncelle(Sayfalar syf)
+        {
+            var sayfa = db.Sayfalars.Where(s => s.Silindi == false && s.SayfaId == syf.SayfaId).FirstOrDefault();
+            sayfa.Baslik = syf.Baslik;
+            sayfa.Icerik = syf.Icerik;
+            sayfa.Aktif = syf.Aktif;
+            db.Sayfalars.Update(sayfa);
+            db.SaveChanges();
+            return RedirectToAction("Sayfalar");
+        }
+        public IActionResult SayfaSil(int id)
+        {
+            var sayfa = db.Sayfalars.Where(s => s.Silindi == false && s.SayfaId == id).FirstOrDefault();
+            sayfa.Silindi = true;
+            db.Sayfalars.Update(sayfa);
+            db.SaveChanges();
+            return RedirectToAction("Sayfalar");
+        }
+
+        //                                              <!-                            Sayfalar END                             -!>
+
+
+        //                                              <!-                            Kategoriler START                             -!>
+
+        public IActionResult Kategoriler()
+        {
+            var kategoriler = db.Kategorilers.Where(k => k.Silindi == false).OrderBy(k => k.KategoriId).ToList();
+
+            return View(kategoriler);
+        }
+        public IActionResult KategoriEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult KategoriEkle(Kategoriler k)
+        {
+            k.Silindi = false;
+            db.Kategorilers.Add(k);
+            db.SaveChanges();
+            return RedirectToAction("Kategoriler");
+        }
+        public IActionResult KategoriGetir(int id)
+        {
+            var kategori = db.Kategorilers.Where(k => k.Silindi == false && k.KategoriId == id).FirstOrDefault();
+
+            return View("KategoriGuncelle", kategori);
+        }
+        public IActionResult KategoriYemekler(int id)
+        {
+            var yemekler = db.YemekTarifleris.Include(k=>k.Kategori).Where(y => y.Silindi == false && y.KategoriId == id).ToList();
+            return View("Tarifler", yemekler);
+        }
+        public IActionResult KategoriGuncelle(Kategoriler ktgr)
+        {
+            var kategori = db.Kategorilers.Where(k => k.Silindi == false && k.KategoriId == ktgr.KategoriId).FirstOrDefault();
+            kategori.Kategoriadi = ktgr.Kategoriadi;
+            kategori.Aktif = ktgr.Aktif;
+            db.Kategorilers.Update(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Kategoriler");
+        }
+        public IActionResult KategoriSil(int id)
+        {
+            var kategori = db.Kategorilers.Where(k => k.Silindi == false && k.KategoriId == id).FirstOrDefault();
+            kategori.Silindi = true;
+            db.Kategorilers.Update(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Kategoriler");
+        }
+
+        //                                              <!-                            Kategoriler END                             -!>
+
+
+        //                                              <!-                            Tarifler START                             -!>
+
+        public IActionResult Tarifler()
+        {
+            var tarifler = db.YemekTarifleris.Include(k=>k.Kategori).Where(t => t.Silindi == false).OrderBy(t => t.TarifId).ToList();
+
+            return View(tarifler);
+        }
+        public IActionResult TarifEkle()
+        {
+            var kategoriler = (from k in db.Kategorilers.Where(k => k.Silindi == false && k.Aktif == true).ToList()
+                select new SelectListItem
+                {
+                    Text = k.Kategoriadi,
+                    Value = k.KategoriId.ToString()
+                }
+            );
+            ViewBag.KategoriId = kategoriler;
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult TarifEkle(YemekTarifleri t)
+        {
+            t.Silindi = false;
+            t.Eklemetarihi = DateTime.Now;
+            db.YemekTarifleris.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Tarifler");
+        }
+        public IActionResult TarifGetir(int id)
+        {
+            var tarif = db.YemekTarifleris.Include(k=>k.Kategori).Where(t => t.Silindi == false && t.TarifId == id).FirstOrDefault();
+            var kategoriler = (from k in db.Kategorilers.Where(k => k.Silindi == false && k.Aktif == true).ToList()
+                               select new SelectListItem
+                               {
+                                   Text = k.Kategoriadi,
+                                   Value = k.KategoriId.ToString()
+                               }
+            );
+            ViewBag.KategoriId = kategoriler;
+            return View("TarifGuncelle", tarif);
+        }
+        public IActionResult TarifYorumlari(int id)
+        {
+            var yorumlar = db.Yorumlars.Where(y => y.Silindi == false && y.TarifId == id).ToList();
+            return View("Yorumlar", yorumlar);
+        }
+        public IActionResult TarifGuncelle(YemekTarifleri trf)
+        {
+            var tarif = db.YemekTarifleris.Where(t => t.Silindi == false && t.KategoriId == trf.KategoriId).FirstOrDefault();
+            tarif.Yemekadi = trf.Yemekadi;
+            tarif.Tarif = trf.Tarif;
+            tarif.Sira = trf.Sira;
+            tarif.KategoriId = trf.KategoriId;
+            tarif.Aktif = trf.Aktif;
+            db.YemekTarifleris.Update(tarif);
+            db.SaveChanges();
+            return RedirectToAction("Tarifler");
+        }
+        public IActionResult TarifSil(int id)
+        {
+            var tarif = db.YemekTarifleris.Where(t => t.Silindi == false && t.KategoriId == id).FirstOrDefault();
+            tarif.Silindi = true;
+            db.YemekTarifleris.Update(tarif);
+            db.SaveChanges();
+            return RedirectToAction("Tarifler");
+        }
+
+        //                                              <!-                            Tarifler END                             -!>
+        //                                              <!-                            Yorumlar START                           -!>
+
+        [HttpGet]
+        public IActionResult Yorumlar()
+        {
+            var yorumlar = db.Yorumlars.Include(t=>t.TarifNavigation).Include(u=>u.Uye).Where(y => y.Silindi == false).OrderByDescending(y => y.Eklemetarihi).ToList();
+
+            return View(yorumlar);
+        }
+        [HttpPost]
+        public IActionResult Yorumlar(string listelemeturu)
+        {
+            var yorumlar = db.Yorumlars.Include(t => t.TarifNavigation).Include(u => u.Uye).Where(y => y.Silindi == false).OrderByDescending(y => y.Eklemetarihi).ToList();
+
+            switch (listelemeturu)
+            {
+                case "Onayli": yorumlar = db.Yorumlars.Include(t => t.TarifNavigation).Include(u => u.Uye).Where(y => y.Silindi == false && y.Aktif == true).OrderByDescending(y => y.Eklemetarihi).ToList(); break;
+                case "Onaysiz": yorumlar = db.Yorumlars.Include(t=>t.TarifNavigation).Include(u=>u.Uye).Where(y => y.Silindi == false && y.Aktif == false).OrderByDescending(y => y.Eklemetarihi).ToList(); break;
+            }
+
+            return View(yorumlar);
+        }
+        public IActionResult Onayla(int id)
+        {
+            var yorum = db.Yorumlars.Where(y => y.Silindi == false && y.YorumId == id).FirstOrDefault();
+            yorum.Aktif = Convert.ToBoolean(-1*Convert.ToInt32(yorum.Aktif)+1);
+            db.Yorumlars.Update(yorum);
+            db.SaveChanges();
+            return RedirectToAction("Yorumlar");
+        }
+        public IActionResult YorumSil(int id)
+        {
+            var yorum = db.Yorumlars.Where(y => y.Silindi == false && y.YorumId == id).FirstOrDefault();
+            yorum.Silindi = true;
+            db.Yorumlars.Update(yorum);
+            db.SaveChanges();
+            return RedirectToAction("Yorumlar");
+        }
+
+        //                                              <!-                            Yorumlar END                             -!>
+        //                                              <!-                            Yorumlar END                             -!>
+
+        public IActionResult CikisYap()
+        {
+            return View();
+        }
+    }
+}
---
+++ OrnekUygulama/Models/YemektarifleriDbContext.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrnekUygulama.Models;
+
+public partial class YemektarifleriDbContext : DbContext
+{
+    public YemektarifleriDbContext()
+    {
+    }
+
+    public YemektarifleriDbContext(DbContextOptions<YemektarifleriDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Kategoriler> Kategorilers { get; set; }
+
+    public virtual DbSet<Kullanicilar> Kullanicilars { get; set; }
+
+    public virtual DbSet<Menuler> Menulers { get; set; }
+
+    public virtual DbSet<Sayfalar> Sayfalars { get; set; }
+
+    public virtual DbSet<YemekTarifleri> YemekTarifleris { get; set; }
+
+    public virtual DbSet<Yorumlar> Yorumlars { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=10.0.0.170;Database=yemektarifleriDB;Trusted_Connection=False;TrustServerCertificate=True;user id=sa;pwd=123@Asd@654;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Kategoriler>(entity =>
+        {
+            entity.HasKey(e => e.KategoriId).HasName("PK__Kategori__1D9181BDCE5CFBF3");
+
+            entity.ToTable("Kategoriler");
+
+            entity.Property(e => e.KategoriId).HasColumnName("kategoriID");
+            entity.Property(e => e.Aktif).HasColumnName("aktif");
+            entity.Property(e => e.Kategoriadi)
+                .HasMaxLength(100)
+                .HasColumnName("kategoriadi");
+            entity.Property(e => e.Silindi).HasColumnName("silindi");
+        });
+
+        modelBuilder.Entity<Kullanicilar>(entity =>
+        {
+            entity.HasKey(e => e.KullaniciId).HasName("PK__Kullanic__848DC54B349FD112");
+
+            entity.ToTable("Kullanicilar");
+
+            entity.Property(e => e.KullaniciId).HasColumnName("kullaniciID");
+            entity.Property(e => e.Adi)
+                .HasMaxLength(100)
+                .HasColumnName("adi");
+            entity.Property(e => e.Aktif).HasColumnName("aktif");
+            entity.Property(e => e.Eposta)
+                .HasMaxLength(100)
+                .HasColumnName("eposta");
+            entity.Property(e => e.Parola)
+                .HasMaxLength(25)
+                .HasColumnName("parola");
+            entity.Property(e => e.Silindi).HasColumnName("silindi");
+            entity.Property(e => e.Soyadi)
+                .HasMaxLength(100)
+                .HasColumnName("soyadi");
+            entity.Property(e => e.Telefon)
+                .HasMaxLength(15)
+                .HasColumnName("telefon");
+            entity.Property(e => e.Yetki).HasColumnName("yetki");
+        });
+
+        modelBuilder.Entity<Menuler>(entity =>
+        {
+            entity.HasKey(e => e.MenuId).HasName("PK__Menuler__3B407E94DFEC4C88");
+
+            entity.ToTable("Menuler");
+
+            entity.Property(e => e.MenuId).HasColumnName("menuID");
+            entity.Property(e => e.Aktif).HasColumnName("aktif");
+            entity.Property(e => e.Baslik)
+                .HasMaxLength(250)
+                .HasColumnName("baslik");
+            entity.Property(e => e.Silindi).HasColumnName("silindi");
+            entity.Property(e => e.Sira).HasColumnName("sira");
+            entity.Property(e => e.Url)
+                .HasMaxLength(255)
+                .HasColumnName("url");
+            entity.Property(e => e.UstId).HasColumnName("ustID");
+        });
+
+        modelBuilder.Entity<Sayfalar>(entity =>
+        {
+            entity.HasKey(e => e.SayfaId).HasName("PK__Sayfalar__F81FC2B25FF0FE4B");
+
+            entity.ToTable("Sayfalar");
+
+            entity.Property(e => e.SayfaId).HasColumnName("sayfaID");
+            entity.Property(e => e.Aktif).HasColumnName("aktif");
+            entity.Property(e => e.Baslik)
+                .HasMaxLength(250)
+                .HasColumnName("baslik");
+            entity.Property(e => e.Icerik)
+                .HasColumnType("ntext")
+                .HasColumnName("icerik");
+            entity.Property(e => e.Silindi).HasColumnName("silindi");
+        });
+
+        modelBuilder.Entity<YemekTarifleri>(entity =>
+        {
+            entity.HasKey(e => e.TarifId).HasName("PK__YemekTar__87FD2128DBBB4615");
+
+            entity.ToTable("YemekTarifleri");
+
+            entity.Property(e => e.TarifId).HasColumnName("tarifID");
+            entity.Property(e => e.Aktif).HasColumnName("aktif");
+            entity.Property(e => e.Eklemetarihi)
+                .HasColumnType("datetime")
+                .HasColumnName("eklemetarihi");
+            entity.Property(e => e.KategoriId).HasColumnName("kategoriID");
+            entity.Property(e => e.Silindi).HasColumnName("silindi");
+            entity.Property(e => e.Sira).HasColumnName("sira");
+            entity.Property(e => e.Tarif)
+                .HasColumnType("ntext")
+                .HasColumnName("tarif");
+            entity.Property(e => e.Yemekadi)
+                .HasMaxLength(250)
+                .HasColumnName("yemekadi");
+
+            entity.HasOne(d => d.Kategori).WithMany(p => p.YemekTarifleris)
+                .HasForeignKey(d => d.KategoriId)
+                .HasConstraintName("FK__YemekTari__kateg__164452B1");
+        });
+
+        modelBuilder.Entity<Yorumlar>(entity =>
+        {
+            entity.HasKey(e => e.YorumId).HasName("PK__Yorumlar__222191BF153BA775");
+
+            entity.ToTable("Yorumlar");
+
+            entity.Property(e => e.YorumId).HasColumnName("yorumID");
+            entity.Property(e => e.Aktif).HasColumnName("aktif");
+            entity.Property(e => e.Eklemetarihi)
+                .HasColumnType("datetime")
+                .HasColumnName("eklemetarihi");
+            entity.Property(e => e.Silindi).HasColumnName("silindi");
+            entity.Property(e => e.TarifId).HasColumnName("tarifID");
+            entity.Property(e => e.UyeId).HasColumnName("uyeID");
+            entity.Property(e => e.Yorum)
+                .HasMaxLength(500)
+                .HasColumnName("yorum");
+
+            entity.HasOne(d => d.TarifNavigation).WithMany(p => p.Yorumlars)
+                .HasForeignKey(d => d.TarifId)
+                .HasConstraintName("FK__Yorumlar__tarifI__1B0907CE");
+
+            entity.HasOne(d => d.Uye).WithMany(p => p.Yorumlars)
+                .HasForeignKey(d => d.UyeId)
+                .HasConstraintName("FK__Yorumlar__uyeID__1BFD2C07");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
